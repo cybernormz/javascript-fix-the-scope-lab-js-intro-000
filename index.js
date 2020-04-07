301fd9f34253125e468535a2dea5c622f4242887
Noa'man Patel <normz@live.co.uk>
2020-04-07 14:48:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
-return animal
-}
+var animal = 'dog'
 
 function myAnimal() {
-var animal = 'dog';
-return animal;
+  var animal = 'dog'
+  return animal
 }
 
 function yourAnimal() {
- // How can we make sure that this function
- // and the above function both pass?
- // P.S.: You can't just hard-code 'cat' below
- return animal
-}
-
-function yourAnimal() {
- var animal = 'cat';
- return animal;
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat'
+  return animal
 }
 
 function add2(n) {
- return n + two
+  const two = 2
+  return n + two
 
- // Feel free to move things around!
- const two = 2
-}
+  // Feel free to move things around!
 
-function add2(n) {
-var two = 2
- return n + two;
 }
 
 var funkyFunction = function() {
- return function() {
-   return "FUNKY!"
-@@ -26,3 +41,4 @@ var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
 
-// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()()
